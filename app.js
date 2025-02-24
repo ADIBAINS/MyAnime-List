df7bc9bea3b2d390b5b54b50c65375921cfd09ba
ADIL BAINS <adibains06@gmail.com>
2025-02-25 00:13:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
             const targetSection = document.getElementById(targetId);
 
             if (targetSection) {
-                const offset = 100;
+                const offset = 96;
                 window.scrollTo({
                     top: targetSection.offsetTop - offset,
                     behavior: "smooth"
