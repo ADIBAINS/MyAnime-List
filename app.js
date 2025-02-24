23ec2f31ee671acd44af9e34148526f171b76832
ADIL BAINS <adibains06@gmail.com>
2025-02-25 00:08:10 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll(".navopt a,homebtn a");
+    navLinks.forEach(link => {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+            const targetId = this.getAttribute("href").substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            if (targetSection) {
+                const offset = 80;
+                window.scrollTo({
+                    top: targetSection.offsetTop - offset,
+                    behavior: "smooth"
+                });
+            }
+        });
+    });
+});
